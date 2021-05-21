31c6203345fcab1b79f9674757a29b48979c4226
Samuel Horn af Rantzien <samuel@samuelhorn.com>
2021-05-21 16:01:39 +0200

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    Sliczone goes here
+    <!-- slice-zone type="page" uid="home" /-->
+  </div>
+</template>
+
+<script>
+// import SliceZone from 'vue-slicezone'
+
+export default {
+  components: {
+    // SliceZone,
+  },
+}
+</script>
