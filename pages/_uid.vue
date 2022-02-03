9e5e0ca9428befc438276e3d6d1dd64ec17ec464
Robin Kenell <robin@wilsoncreative.se>
2022-02-03 09:24:54 +0100

    add og and twitter meta

--- pages/_uid.vue
+++ pages/_uid.vue
@@ -29,8 +29,33 @@ export default {
           content: this.doc.data.metaDescription ?? '',
         },
         {
+          hid: 'og:title',
+          name: 'og:title',
+          content: this.doc.data.metaTitle ?? this.doc.data.title[0].text,
+        },
+        {
+          hid: 'og:description',
+          name: 'og:description',
+          content: this.doc.data.metaDescription ?? '',
+        },
+        {
           hid: 'og:image',
           name: 'og:image',
+          content: this.doc.data.metaImage.url ?? '',
+        },
+        {
+          hid: 'twitter:title',
+          name: 'twitter:title',
+          content: this.doc.data.metaTitle ?? this.doc.data.title[0].text,
+        },
+        {
+          hid: 'twitter:description',
+          name: 'twitter:description',
+          content: this.doc.data.metaDescription ?? '',
+        },
+        {
+          hid: 'twitter:image',
+          name: 'twitter:image',
           content: this.doc.data.metaImage.url ?? '',
         },
       ],
